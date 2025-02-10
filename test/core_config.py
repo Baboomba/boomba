dab9beb18ea40feddd86cb8ee676fbd29206303a
babamba <bach0918@outlook.kr>
2025-02-10 17:46:23 +0900

    [TEST]Add test for core.config.py

--- test/core_config.py
+++ test/core_config.py
@@ -5,74 +5,56 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 import unittest
 from unittest.mock import patch, MagicMock
-import configparser
-from core.builder.config import ConfigFileReader, ConfigLoader
+
+from tempfile import NamedTemporaryFile
+from boomba.core.config import Config
+from boomba.core.constants import MOCK_CONFIG
+
 
 class TestConfigFileReader(unittest.TestCase):
     
-    def test_get_conf_path_valid(self):
-        config_reader = ConfigFileReader("db")
-        path = config_reader._get_conf_path("db")
-        expected_path = Path("./config/db.ini")
-        self.assertEqual(path, expected_path)
+    def test_load_non_empty(self):
+        content = 'FIRST_VAR="first_value"; SECOND_VAR="second_value"'
+        return_value = {'FIRST_VAR': 'first_value', 'SECOND_VAR': 'second_value'}
+
+        with NamedTemporaryFile(mode='w+t', delete=False) as f:
+            f.write(content)
+            tmp_path = Path(f.name)
+        
+        try:
+            config = Config(tmp_path)
+            self.assertEqual(config._vars, return_value)
+        finally:
+            tmp_path.unlink()
     
-    @patch("pathlib.Path.exists")
-    def test_get_conf_path_invalid(self, mock_exists):
-        mock_exists.return_value = False
+    def test_load_empty(self):
+        # required non-exist-path
+        path = Path('not a path!')
+        if path.exists():
+            raise Exception(f"path exists. {path}")
         
-        with self.assertRaises(FileNotFoundError):
-            config_reader = ConfigFileReader("It is not a file name!!!")
+        config = Config(path)
+        self.assertEqual(config._vars, MOCK_CONFIG)
     
-    @patch("pathlib.Path.exists")
-    @patch.object(configparser.ConfigParser, "read")
-    def test_read_file_valid(self, mock_read, mock_exists):
-        mock_exists.return_value = True
-        mock_read.return_value = None
-        
-        config_reader = ConfigFileReader("db")
-        config_reader._read_file("db")
-        
-        mock_read.assert_called_once()
+    def test_set_attr(self):
+        first_flag = False
+        second_flag = False
 
-    def test_content_valid(self):
-        conf_reader = ConfigFileReader("db")
-        conf_reader._conf.read_dict({
-            "section1": {"key1": "value1", "key2": "value2"}
-        })
+        content = 'FIRST_VAR="first_value"; SECOND_VAR="second_value"'
         
-        content = conf_reader.content("section1")
+        with NamedTemporaryFile(mode='w+t', delete=False) as f:
+            f.write(content)
+            tmp_path = Path(f.name)
         
-        self.assertEqual(content, {"key1": "value1", "key2": "value2"})
-    
-    def test_content_invalid_section(self):
-        conf_reader = ConfigFileReader("db")
-        conf_reader._conf.read_dict({
-            "section1": {"key1": "value1", "key2": "value2"}
-        })
-        
-        with self.assertRaises(KeyError):
-            conf_reader.content("non_existing_section")
+        try:
+            config = Config(tmp_path)
 
-
-class TestConfigLoader(unittest.TestCase):
-
-    @patch("os.path.exists")
-    @patch.object(configparser.ConfigParser, "read")
-    def test_dynamic_attr_setting(self, mock_read, mock_exists):
-        mock_exists.return_value = True
-        mock_read.return_value = None
-        
-        config_loader = ConfigLoader("db", "section1")
-        
-        # ConfigLoader가 동적으로 속성을 설정했는지 확인
-        config_loader._conf.read_dict({
-            "section1": {"key1": "value1", "key2": "value2"}
-        })
-        
-        # 속성이 객체에 추가되었는지 확인
-        self.assertEqual(config_loader.key1, "value1")
-        self.assertEqual(config_loader.key2, "value2")
-
-
-if __name__ == "__main__":
-    unittest.main()
+            if config.first_var == 'first_value':
+                first_flag = True
+            
+            if config.second_var == 'second_value':
+                second_flag = True
+            
+            self.assertTrue(all([first_flag, second_flag]))
+        finally:
+            tmp_path.unlink()

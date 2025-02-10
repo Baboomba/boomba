65027bd3d0be4bc758e80f36b66bc51c1ae7e3bc
babamba <bach0918@outlook.kr>
2025-02-10 17:45:38 +0900

    [REFACTOR]Injected dependency of conf_path

--- boomba/core/config.py
+++ boomba/core/config.py
@@ -1,25 +1,28 @@
 from typing import Dict, Any
 from pathlib import Path
 from boomba.core.constants import CONF_PATH, MOCK_CONFIG
-            
+
 
 class Config:
-    is_mock: bool = False
+    is_mock: bool
+    conf_path: Path
     
-    def __init__(self) -> None:
+    def __init__(self, conf_path: Path=None) -> None:
+        self.is_mock = False
+        self.conf_path = conf_path or Path(CONF_PATH)
         self._vars = self._load()
         self._set_attr()
-        
+    
     def __repr__(self) -> str:
         return (
-            f"Config(var={self._vars}, is_mock={self._mock})"
+            f"Config(var={self._vars}, is_mock={self.is_mock})"
         )
     
     def _load(self) -> Dict[str, Any]:
-        if not Path(CONF_PATH).exists():
+        if not self.conf_path.exists():
             return self._temp_config()
-                
-        with open(CONF_PATH) as f:
+        
+        with open(str(self.conf_path)) as f:
                 conf = f.read()
         
         vars = {}
